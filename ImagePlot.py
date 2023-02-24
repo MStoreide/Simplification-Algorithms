504ee93d2c24059cfb6dbe6059fadf9de3450b9a
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-02-24 15:29:13 +0100

    Image Update

--- ImagePlot.py
+++ ImagePlot.py
@@ -1,16 +1,28 @@
 import matplotlib.pyplot as plt
 import os
-import random
 
 # Set the directory containing the images
-image_dir = r"D:\Datasets\Paper - Simplification\Metro Snapshots\UNMEC"
+image_dir = r"D:\Datasets\Paper - Simplification\Metro Snapshots\RNMEC"
 
-# Get a list of all the image file names in the directory
-image_files = os.listdir(image_dir)
-
-#Fix this so that it gets them in the right order.
-# Select 16 random images from the directory
-selected_images = random.sample(image_files, 16)
+# List the image file names in the order you want to display them
+image_files = [
+    "RNMEC01.png",
+    "RNMEC02.png",
+    "RNMEC03.png",
+    "RNMEC04.png",
+    "RNMEC05.png",
+    "RNMEC06.png",
+    "RNMEC07.png",
+    "RNMEC08.png",
+    "RNMEC09.png",
+    "RNMEC10.png",
+    "RNMEC11.png",
+    "RNMEC12.png",
+    "RNMEC13.png",
+    "RNMEC14.png",
+    "RNMEC15.png",
+    "RNMEC16.png",
+]
 
 # Create a 4x4 grid of subplots
 fig, axs = plt.subplots(4, 4, figsize=(8,8))
@@ -18,7 +30,7 @@ fig, axs = plt.subplots(4, 4, figsize=(8,8))
 # Iterate over the subplots and plot each image
 for i, ax in enumerate(axs.flat):
     # Load the image file and plot it in the current subplot
-    image_file = os.path.join(image_dir, selected_images[i])
+    image_file = os.path.join(image_dir, image_files[i])
     image = plt.imread(image_file)
     ax.imshow(image)
     ax.axis('off')
