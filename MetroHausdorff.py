f4c829eb59f39cf6bca45a78097c3662fae15364
Markus <markus.storeide@gmail.com>
2023-02-27 20:51:03 +0100

    Hausdorff Notes

--- MetroHausdorff.py
+++ MetroHausdorff.py
@@ -5,9 +5,25 @@ import pymeshlab
 #Using pymeshlab, we apply the Huasdorff distance filter to all meshes compared to the original (SM_Randomized).
 #Sample faces at the total triangle count of the original mesh.
 
-#pym.load_new_mesh('D:\Datasets\Paper_Simplification\Blender_Topology_Deform\SM_Randomized.obj')
+#   ms = pymeshlab.MeshSet() # Check what this does. 
 
-#pym.get_hausdorff_distance()
+#baseline = pym.load_new_mesh('D:\Datasets\Paper_Simplification\Baseline\SM.obj')
+#simp.1 = pym.load_new_mesh('D:\Datasets\Paper_Simplification\Decimation\SMD.obj') #This is with Decimation. Change to others.
+#Add all 16 meshes here.  
+
+#pym.get_hausdorff_distance():
+#   baseline : int = 5050050 (Total triangles in THIS mesh) ?
+#   simp.1 = int = 40404040 (Total triangles in THIS mesh) ?
+#   savesample : bool = True
+#   samplevert : bool = False
+#   sampleedge : bool = False
+#   samplefauxedge : bool = False
+#   sampleface : bool = True
+#   samplenum : int = (Total triangles in baseline)
+#   maxdist : int = 50%
+
+# ms.save_current_mesh? ???
+
 
 pymeshlab.print_filter_list()
 
