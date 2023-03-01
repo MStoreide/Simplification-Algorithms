d3303f0701c0ee07804e6662af02064703c803ea
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-01 15:58:09 +0100

    Vertex Clustering Addition

--- Open3DVertexClustering/NUMVC.py
+++ Open3DVertexClustering/NUMVC.py
@@ -7,6 +7,57 @@ print(
     f'Baseline mesh has {len(NUMVC.vertices)} vertices and {len(NUMVC.triangles)} triangles'
 )
 
+#10 is approximately dividing by 197
+voxel_size = max(NUMVC.get_max_bound() - NUMVC.get_min_bound()) / 214 
+print(f'voxel_size = {voxel_size:e}')                                
+mesh_smp10 = NUMVC.simplify_vertex_clustering(                        
+    voxel_size=voxel_size,                                          
+    contraction=o3d.geometry.SimplificationContraction.Average)
+print(
+    f'Simplification stage 10 has {len(mesh_smp10.vertices)} vertices and {len(mesh_smp10.triangles)} triangles'
+)
+
+o3d.io.write_triangle_mesh(r"G:\Markus' Folder\Business Backup\Datasets\Paper_Simplification\Vertex Clustering\NUMVC\NUMVC10.obj", mesh_smp10)
+
+
+#11 is approximately dividing by 197
+voxel_size = max(NUMVC.get_max_bound() - NUMVC.get_min_bound()) / 197 
+print(f'voxel_size = {voxel_size:e}')                                
+mesh_smp11 = NUMVC.simplify_vertex_clustering(                        
+    voxel_size=voxel_size,                                          
+    contraction=o3d.geometry.SimplificationContraction.Average)
+print(
+    f'Simplification stage 11 has {len(mesh_smp11.vertices)} vertices and {len(mesh_smp11.triangles)} triangles'
+)
+
+o3d.io.write_triangle_mesh(r"G:\Markus' Folder\Business Backup\Datasets\Paper_Simplification\Vertex Clustering\NUMVC\NUMVC11.obj", mesh_smp11)
+
+
+#12 is approximately dividing by 183
+voxel_size = max(NUMVC.get_max_bound() - NUMVC.get_min_bound()) / 183 
+print(f'voxel_size = {voxel_size:e}')                                
+mesh_smp12 = NUMVC.simplify_vertex_clustering(                        
+    voxel_size=voxel_size,                                          
+    contraction=o3d.geometry.SimplificationContraction.Average)
+print(
+    f'Simplification stage 12 has {len(mesh_smp12.vertices)} vertices and {len(mesh_smp12.triangles)} triangles'
+)
+
+o3d.io.write_triangle_mesh(r"G:\Markus' Folder\Business Backup\Datasets\Paper_Simplification\Vertex Clustering\NUMVC\NUMVC12.obj", mesh_smp12)
+
+
+#13 is approximately dividing by 170
+voxel_size = max(NUMVC.get_max_bound() - NUMVC.get_min_bound()) / 170 
+print(f'voxel_size = {voxel_size:e}')                                
+mesh_smp13 = NUMVC.simplify_vertex_clustering(                        
+    voxel_size=voxel_size,                                          
+    contraction=o3d.geometry.SimplificationContraction.Average)
+print(
+    f'Simplification stage 13 has {len(mesh_smp13.vertices)} vertices and {len(mesh_smp13.triangles)} triangles'
+)
+
+o3d.io.write_triangle_mesh(r"G:\Markus' Folder\Business Backup\Datasets\Paper_Simplification\Vertex Clustering\NUMVC\NUMVC13.obj", mesh_smp13)
+
 #14 is approximately dividing by 155
 voxel_size = max(NUMVC.get_max_bound() - NUMVC.get_min_bound()) / 155
 print(f'voxel_size = {voxel_size:e}')                                
@@ -44,3 +95,15 @@ print(
 )
 
 o3d.io.write_triangle_mesh(r"G:\Markus' Folder\Business Backup\Datasets\Paper_Simplification\Vertex Clustering\NUMVC\NUMVC16.obj", mesh_smp16)
+
+#Function version 
+#def vertex.clustering(mesh):
+#    mesh = o3d.io.read_triangle_mesh()
+#    voxel_size = max(mesh.get_max_bound() - mesh.get_min_bound()) / 111 
+#    print(f'voxel_size = {voxel_size:e}')                                
+#    mesh_smp = mesh.simplify_vertex_clustering(                        
+#    voxel_size=voxel_size,                                           
+#    contraction=o3d.geometry.SimplificationContraction.Average)      
+#    print(
+#    f'Simplification stage 16 has {len(mesh_smp.vertices)} vertices and {len(mesh_smp.triangles)} triangles')
+#    o3d.io.write_triangle_mesh(r"G:\Markus' Folder\Business Backup\Datasets\Paper_Simplification\Vertex Clustering\NUMVC\NUMVC16.obj", mesh_smp)
