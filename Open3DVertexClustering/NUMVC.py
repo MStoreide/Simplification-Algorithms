d713e55dbd23aa4f7fe3f37365b735e8885d75f6
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-01 16:15:00 +0100

    Voxelnumbers

--- Open3DVertexClustering/NUMVC.py
+++ Open3DVertexClustering/NUMVC.py
@@ -7,7 +7,21 @@ print(
     f'Baseline mesh has {len(NUMVC.vertices)} vertices and {len(NUMVC.triangles)} triangles'
 )
 
-#10 is approximately dividing by 197
+
+#9 is approximately dividing by 214
+voxel_size = max(NUMVC.get_max_bound() - NUMVC.get_min_bound()) / 227 
+print(f'voxel_size = {voxel_size:e}')                                
+mesh_smp9 = NUMVC.simplify_vertex_clustering(                        
+    voxel_size=voxel_size,                                          
+    contraction=o3d.geometry.SimplificationContraction.Average)
+print(
+    f'Simplification stage 9 has {len(mesh_smp9.vertices)} vertices and {len(mesh_smp9.triangles)} triangles'
+)
+
+o3d.io.write_triangle_mesh(r"G:\Markus' Folder\Business Backup\Datasets\Paper_Simplification\Vertex Clustering\NUMVC\NUMVC9.obj", mesh_smp9)
+
+
+#10 is approximately dividing by 214
 voxel_size = max(NUMVC.get_max_bound() - NUMVC.get_min_bound()) / 214 
 print(f'voxel_size = {voxel_size:e}')                                
 mesh_smp10 = NUMVC.simplify_vertex_clustering(                        
