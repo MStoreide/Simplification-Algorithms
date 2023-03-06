efb274751c424ab67dc9df00fbe54c3b1eab0724
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-06 09:59:21 +0100

    pym implementation

--- MetroHausdorff.py
+++ MetroHausdorff.py
@@ -1,4 +1,5 @@
 import pymeshlab as pym
+import numpy as np
 
 #Hausdorff Distances for each mesh
 #Make sure the pymeshlab conda environment is activated (for Python 3.7)
@@ -6,8 +7,10 @@ import pymeshlab as pym
 #Using pymeshlab, we apply the Huasdorff distance filter to all meshes compared to the original (SM_Randomized).
 #Sample faces at the total triangle count of the original mesh.
 
-ms = pym.MeshSet() # Creates a set where we can load our meshes into. ?
-pym.print_filter_list()
+pym.search('Hausdorff')
+
+#ms = pym.MeshSet() # Creates a container where we can load our meshes into.
+#ms.load_new_mesh('G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Baselines\SMB.obj')
 
 #baseline = pym.load_new_mesh('G:\Datasets\Paper_Simplification\Baseline\SM.obj')
 #simp1 = pym.load_new_mesh('G:\Datasets\Paper_Simplification\Decimation\SMD\SMD16.obj')
