c5c7f1615cc142c0c4cb6b620f0bc0905501d835
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-06 10:15:51 +0100

    pym bugs

--- MetroHausdorff.py
+++ MetroHausdorff.py
@@ -7,10 +7,8 @@ import numpy as np
 #Using pymeshlab, we apply the Huasdorff distance filter to all meshes compared to the original (SM_Randomized).
 #Sample faces at the total triangle count of the original mesh.
 
-pym.search('Hausdorff')
-
-#ms = pym.MeshSet() # Creates a container where we can load our meshes into.
-#ms.load_new_mesh('G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Baselines\SMB.obj')
+ms = pym.MeshSet() # Creates a container where we can load our meshes into.
+ms.load_new_mesh(r'G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Baselines\SMB.obj')
 
 #baseline = pym.load_new_mesh('G:\Datasets\Paper_Simplification\Baseline\SM.obj')
 #simp1 = pym.load_new_mesh('G:\Datasets\Paper_Simplification\Decimation\SMD\SMD16.obj')
