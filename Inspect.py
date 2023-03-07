308301d95b5513c8f9c1eef18c8cd0233f889d51
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-07 13:28:15 +0100

    Added QEM

--- Inspect.py
+++ Inspect.py
@@ -1,5 +1,6 @@
 import inspect
 import open3d as o3d
+import pymeshlab as pym
 
 source_D = inspect.getsource(o3d.simplify_quadric_decimation)
 print("Source Code for Decimation")
@@ -8,3 +9,7 @@ print(source_D)
 source_VC = inspect.getsource(o3d.simplify_vertex_clustering)
 print("Source Code for Vertex Clustering")
 print(source_VC)
+
+source_QEM = inspect.getsource(pym.QuadricSimplification)
+print("Sourcecode for QEM")
+print(source_QEM)
