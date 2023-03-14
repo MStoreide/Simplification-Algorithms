bdc91c170f627c96237b5e216e9c168350ec547d
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-14 14:50:26 +0100

    Pandas Dataframe

--- OBJ to Array.py
+++ OBJ to Array.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import numpy as np
 import pandas as pd
+import  matplotlib as plt
 
 
 #objdir = ("Directory") #Directory for objs
@@ -15,24 +16,27 @@ import pandas as pd
 
 # Copy obj to "Copy" directory, Rename original to .txt
 
-objFile = (r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays\SMEC16.obj")
-dst = ("G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays")
-base = os.path.splitext(objFile)[0]
-objCopy = shutil.copyfile(objFile, dst)
-copyfile = os.rename(objFile, base + "Copy.obj")
-os.rename(objFile, base + ".txt")
+#  objFile = (r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays\SMEC16.obj")
+#dst = ("G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays")
+#  base = os.path.splitext(objFile)[0]
+#objCopy = shutil.copyfile(objFile, dst)
+#copyfile = os.rename(objFile, base + "Copy.obj")
+#  os.rename(objFile, base + ".txt")
 
-objArray = np.loadtxt(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays\SMEC16.txt", skiprows = (4), max_rows= (300), usecols=(1,2,3))
+objArray = np.loadtxt(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays\SMEC16.txt", skiprows = (4), max_rows=(1687), usecols=(1,2,3)) #1687 are all the vertices. Code to only go this far, Index 0 = v
+objDF = pd.DataFrame(objArray, columns = ['X','Y','Z'])
+#vertexnum = len(objArray) # Function could be here
 
-print(len(objArray))
-
-# value : int
+print(f"OBJ has {len(objArray)} vertices")
 
 #For loop to print something?
 
 #for value in (objArray):
 #    if value >= int(1):
 #        print(value)
+
+#objArray.plot()
+#plt.show()
 
 
 
