a6dc15a9b370208c696f40b5484155eff54be80e
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-14 11:18:49 +0100

    Copying and Renaming

--- OBJ to Array.py
+++ OBJ to Array.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 import numpy as np
+import pandas as pd
+
 
 #objdir = ("Directory") #Directory for objs
 
@@ -10,11 +13,17 @@ import numpy as np
 #        os.rename(f, base + ".txt")
 
 
+# Copy obj to "Copy" directory, Rename original to .txt
+
 objFile = (r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays\SMEC16.obj")
+dst = ("G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays")
 base = os.path.splitext(objFile)[0]
+objCopy = shutil.copyfile(objFile, dst)
+copyfile = os.rename(objFile, base + "Copy.obj")
 os.rename(objFile, base + ".txt")
 
-objArray = np.loadtxt(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays\SMEC16.txt", skiprows = (4), usecols=(1,2,3))
+objArray = np.loadtxt(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\OBJ Arrays\SMEC16.txt", skiprows = (4), max_rows= (300), usecols=(1,2,3))
+
 print(len(objArray))
 
 # value : int
