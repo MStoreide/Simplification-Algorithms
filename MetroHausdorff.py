adbf525c2134c182eae53a4e5b7705f360ac9f95
Markus <markus.storeide@gmail.com>
2023-03-15 12:48:28 +0100

    Working Hausdorff

--- MetroHausdorff.py
+++ MetroHausdorff.py
@@ -5,8 +5,8 @@ import numpy as np
 #Sample faces at the total triangle count of the original mesh.
 
 ms = pym.MeshSet() # Creates a container where we can load our meshes into.
+ms.load_new_mesh('SMD16.obj')
 ms.load_new_mesh('SMB.obj')
-#ms.load_new_mesh('SMD16.obj')
 
 baseline = ms.current_mesh()
 
@@ -15,7 +15,7 @@ print("Current mesh is: ", (ms.current_mesh_id()))
 samples = baseline.face_number()
 print(f"Current mesh has", samples, "faces")
 
-ms.get_hausdorff_distance(sampledmesh = 0, targetmesh = 1, savesample=True, sampleface=True, samplenum = (samples), maxdist = pym.Percentage(50))
+print(ms.get_hausdorff_distance(sampledmesh = 1, targetmesh = 0, savesample=True, sampleface=True, samplenum = (samples), maxdist = pym.Percentage(50)))
 
 # ms.save_current_mesh? ???
 
