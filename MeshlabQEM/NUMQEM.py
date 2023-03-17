9db342e49f6f2cbf39d94ae9f2a1e685c8f94e96
Markus <markus.storeide@gmail.com>
2023-03-17 11:36:55 +0100

    QEM Simp Stages

--- MeshlabQEM/NUMQEM.py
+++ MeshlabQEM/NUMQEM.py
@@ -17,6 +17,21 @@ ms.meshing_decimation_quadric_edge_collapse(targetfacenum = (int(baselinetris*0.
 QEM1faces = ms[1].face_number()
 print("QEM1 has", QEM1faces, "faces")
 
-ms.save_current_mesh(file_name='SMBQEM1.obj')
+ms.save_current_mesh(file_name='SMBQEM1.obj') #Select a filepath for this
+
+# Simp Stage 2
+
+ms.add_mesh(baseline, mesh_name='SMBQEM2')
+print("Current mesh is ", ms.current_mesh_id()) #Needs to be the duplicated mesh at this point
+
+ms.meshing_decimation_quadric_edge_collapse(targetfacenum = (int(baselinetris*0.5))) # Fix correct number
+
+QEM2faces = ms[2].face_number()
+print("QEM2 has", QEM2faces, "faces")
+
+ms.save_current_mesh(file_name='SMBQEM2.obj') #Select a filepath for this
+
+
+#Add remaining 14 stages
 
 ms.__str__()
