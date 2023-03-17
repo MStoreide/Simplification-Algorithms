f94048d89eeb02bdb62fcddec2754d7eae044cb0
Markus <markus.storeide@gmail.com>
2023-03-17 11:24:08 +0100

    Working QEM process

--- MeshlabQEM/NUMQEM.py
+++ MeshlabQEM/NUMQEM.py
@@ -8,10 +8,15 @@ ms.load_new_mesh('SMB.obj')
 baseline = ms.current_mesh()
 baselinetris = baseline.face_number()
 
-ms.show_polyscope()
-
-print(ms.current_mesh)
-
 #Duplicate baseline
+ms.add_mesh(baseline, mesh_name='SMBQEM1')
+print("Current mesh is ", ms.current_mesh_id()) #Needs to be the duplicated mesh at this point
 
-#ms.meshing_decimation_quadric_edge_collapse(targetfacenum = (baselinetris*PERCENTAGE), )
+ms.meshing_decimation_quadric_edge_collapse(targetfacenum = (int(baselinetris*0.975)))
+
+QEM1faces = ms[1].face_number()
+print("QEM1 has", QEM1faces, "faces")
+
+ms.save_current_mesh(file_name='SMBQEM1.obj')
+
+ms.__str__()
