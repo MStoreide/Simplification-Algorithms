80eddaf169440c861c2cbc3d05a4fbcf5be23517
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-21 13:31:17 +0100

    PLY Inspection

--- PLYInspection.py
+++ PLYInspection.py
@@ -1,11 +1,17 @@
 import open3d as o3d
 import pandas as pd
+import numpy as np
 
 print("Reading Point Cloud")
 
 pc = o3d.io.read_point_cloud(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Metro Data\U PointClouds\UMD\UMD16.ply")
 
 print(pc)
+print(np.asarray(pc.points))
+
+pcdf = pd.DataFrame(pc, columns=['X', 'Y', 'Z', 'R', 'G', 'B'])
+
+#   Visualization of whole point cloud
 
 #o3d.visualization.draw_geometries([pc],
 #                                  zoom=0.3412,
@@ -21,3 +27,13 @@ print(pc)
 # - Distance between selected points (min, max, mean)
 # - Different clustering approaches 
 # - Plane segmentation
+
+
+# Calculate Chamfer Distance
+
+def chamf_dist():
+
+
+    dist = pc.compute_point_cloud_distance()
+
+#print(dist)
