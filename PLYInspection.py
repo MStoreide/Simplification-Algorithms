9db1ef319063bd9e91b3e59ba6a3adc00d5d6f22
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-03-22 15:18:16 +0100

    PLY Updates

--- PLYInspection.py
+++ PLYInspection.py
@@ -4,20 +4,23 @@ import numpy as np
 
 print("Reading Point Cloud")
 
-pc = o3d.io.read_point_cloud(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Metro Data\U PointClouds\UMD\UMD16.ply")
+base_pc = o3d.io.read_point_cloud(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Metro Data\U PointClouds\UMD\UMD16.ply")
 
 print(pc)
 
-plyarray = np.loadtxt(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Metro Data\U PointClouds\UMD\UMD162.txt", skiprows = (18), usecols=(1, 2, 3, 7, 8, 9))
+# Change to read the point cloud
+plyarray = np.loadtxt(r"G:\Markus_Folder\Business Backup\Datasets\Paper_Simplification\Metro Data\U PointClouds\UMD\UMD16.txt", 
+                    skiprows = (18), 
+                    usecols=(1, 2, 3, 7, 8, 9))
 plyDF = pd.DataFrame(plyArray, columns = ['X','Y','Z', 'R', 'G', 'B'])
 
 #   Visualization of whole point cloud
 
-#o3d.visualization.draw_geometries([pc],
-#                                  zoom=0.3412,
-#                                  front=[0.4257, -0.2125, -0.8795],
-#                                  lookat=[2.6172, 2.0475, 1.532],
-#                                  up=[-0.0694, -0.9768, 0.2024])
+o3d.visualization.draw_geometries([base_pc],
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
 
 #What info do we want?
 # - Bounding Box
