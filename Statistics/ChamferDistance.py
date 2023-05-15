fa59e5218f77c36c9a1189f5d9fd454b4313f410
MStoreide <104425894+MStoreide@users.noreply.github.com>
2023-05-15 09:36:32 +0200

    Chamfer

--- Statistics/ChamferDistance.py
+++ Statistics/ChamferDistance.py
@@ -7,5 +7,6 @@ pc2 = o3d.io.read_point_cloud("../../test_data/fragment.ply")
 
 dists = pc1.compute_point_cloud_distance(pc2)
 dists_ar = np.asarray(dists)
-ind = np.where(dists_ar > 0.01)[0]
+#ind = np.where(dists_ar > 0.01)[0]
+print(dist_ar)
 print(ind)
